97389791bc673f2f63ccd24337d41e81c916cac1
gregusk89@gmail.com <gregusk89@gmail.com>
2023-11-14 17:25:50 +0100

    telizold

---
+++ database/migrations/2023_11_12_144401_create_page_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 128);
+            $table->string('slug')->charset('ascii');
+            /*$table->string('slug')->unique()->default(function ($value) {
+                return Str::slug($value->title);
+            });*/
+            $table->text('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pages');
+    }
+};
+
+
